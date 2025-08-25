92d8407e4c4eca0f33e165d3abf117ca13049452
Maryline Kouamé-Kodia <maryline.kouame-kodia@koralplay.com>
2025-08-25 17:56:21 +0200

    autre fix

--- model/src/main/java/com/waraloyer/client/model/Rental.java
+++ model/src/main/java/com/waraloyer/client/model/Rental.java
@@ -1,34 +1,60 @@
 package com.waraloyer.client.model;
 
-import jakarta.persistence.*;
-import lombok.Data;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
 
 @Entity
 @Table(name = "rental")
-@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Rental {
+
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY) // S'assure que l'ID est généré automatiquement
-    private Long id; // <-- Correction : le type est maintenant Long
-    @ManyToOne
-    @JoinColumn(name = "user_id", referencedColumnName = "id")
-    private User user;
-    @ManyToOne
-    @JoinColumn(name = "property_id", referencedColumnName = "id")
-    private Property property;
-    @ManyToOne
-    @JoinColumn(name = "locataire_id", referencedColumnName = "id")
-    private Tenant locataire; // J'ai renommé en locataire pour correspondre au script SQL
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     private LocalDate dueDate;
     private BigDecimal amountDue;
     private LocalDate paymentDate;
     private String status;
-    private Boolean isReminderSent;
-    private LocalDate lastReminderSentDate;
-    private Boolean isRelanceSent;
-    private LocalDate lastRelanceSentDate;
     private String comments;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean isReminderSent = false; // Initialisation du champ
+
+    private LocalDate lastReminderSentDate;
+
+    @Column(columnDefinition = "boolean default false")
+    private boolean isRelanceSent = false; // Initialisation du champ
+
+    private LocalDate lastRelanceSentDate;
+    private BigDecimal monthlyCosts;
+    private BigDecimal taxes;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "property_id", nullable = false)
+    private Property property;
+
+    @ManyToOne
+    @JoinColumn(name = "locataire_id", nullable = false)
+    private Tenant tenant;
 }
