8430cf7cd5a6f9c8faba42f2729f4dbfb9d18c21
Maryline Kouamé-Kodia <maryline.kouame-kodia@koralplay.com>
2025-08-25 11:51:16 +0200

    reboot all BDD

--- config/src/main/java/com/waraloyer/client/config/SecurityConfig.java
+++ config/src/main/java/com/waraloyer/client/config/SecurityConfig.java
@@ -60,5 +60,5 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception {
         return authenticationConfiguration.getAuthenticationManager();
     }
-    
+
 }
