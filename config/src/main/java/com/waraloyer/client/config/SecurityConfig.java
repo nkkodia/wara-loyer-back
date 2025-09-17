c1c4b54cda4ff34fa27eb01d721c3a6d4601d09a
Maryline Kouamé-Kodia <maryline.kouame-kodia@koralplay.com>
2025-09-17 10:52:46 +0200

    ça

--- config/src/main/java/com/waraloyer/client/config/SecurityConfig.java
+++ config/src/main/java/com/waraloyer/client/config/SecurityConfig.java
@@ -47,8 +47,9 @@ public class SecurityConfig {
                                 "/api/tenant-messages/report-problem/**",
                                 "/api/auth/create-password"
                         ).permitAll()
+                        .requestMatchers("/api/auth/subscription").authenticated()
                         .requestMatchers("/api/properties/**","/api/dashboard/**", "/api/rentals/**", "/api/tenants/**",
-                                "/api/config/**", "/api/reports/**", "/api/sms/**", "/api/auth/subscription"
+                                "/api/config/**", "/api/reports/**", "/api/sms/**"
                         ).authenticated()
                         .requestMatchers(
                                 "/api/expenses/**"
